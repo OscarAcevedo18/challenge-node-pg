ec2e8d7b6c2924197705ee5f6fe19d406a885cce
OscarAcevedo18 <oacevedovilches@gmail.com>
2022-12-31 13:05:28 -0300

    feature: new endpoint post is created to update records

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.listen(process.env.PORT, console.log("SERVIDOR ENCENDIDO"));
 
 CsbInspector();
 
+const errorServer = {
+  status: 500,
+  statusText: "error",
+  text: "Error interno del servidor",
+};
+
 // Devolviendo el html
 
 app.get("/", (req, res) => {
@@ -31,5 +37,32 @@ app.get("/posts", async (req, res) => {
     res.json(likes);
   } catch (error) {
     res.json({ message: "El recurso no está disponible" });
+  }
+});
+
+// API REST con PostgreSQL(POST)
+app.post("/posts", async (req, res) => {
+  try {
+    const payload = req.body;
+    const resultDuplicate = await duplicatePost(payload);
+    if (
+      payload.id === "" ||
+      payload.titulo === "" ||
+      payload.url === "" ||
+      payload.descripcion === ""
+    ) {
+      res.send("Falta Ingresar Datos");
+      // console.log("Falta Ingresar Datos");
+    } else if (resultDuplicate[0].num > 0) {
+      res.send("Registro Ya Existente");
+      // console.log("Registro Ya Existente");
+    } else {
+      await addPost(payload);
+      res.send("Post Agregado con Éxito");
+    }
+  } catch (error) {
+    res
+      .status(errorServer.status)
+      .send({ status: errorServer.statusText, data: errorServer.text });
   }
 });
